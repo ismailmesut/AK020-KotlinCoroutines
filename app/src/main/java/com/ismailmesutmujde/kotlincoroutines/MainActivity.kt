f75dac1ad9e75cc8cc2ef090f11ee2daf61c3dee
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-06 19:12:49 +0300

    updated: GlobalScope vs runBlocking

--- app/src/main/java/com/ismailmesutmujde/kotlincoroutines/MainActivity.kt
+++ app/src/main/java/com/ismailmesutmujde/kotlincoroutines/MainActivity.kt
@@ -3,7 +3,10 @@ package com.ismailmesutmujde.kotlincoroutines
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.plus
+import kotlinx.coroutines.runBlocking
 
 
 class MainActivity : AppCompatActivity() {
@@ -23,5 +26,27 @@ class MainActivity : AppCompatActivity() {
         }
          */
 
+        // Scope
+        // Global Scope, runBlocking, CoroutineScope
+
+        /*
+        // runBlocking
+        println("run blocking start")
+        runBlocking {
+            launch {
+                delay(5000)
+                println("run blocking")
+            }
+        }
+        println("run blocking end")
+        */
+
+        // Global Scope
+        println("global scope start")
+        GlobalScope.launch {
+            delay(5000)
+            println("global scope")
+        }
+        println("global scope end")
     }
 }
